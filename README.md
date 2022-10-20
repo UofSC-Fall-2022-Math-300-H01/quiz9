24bffb4b723afa1e1ff16659caf968031a0f7b11
Matthew Robert Ballard <100034030+mattrobball@users.noreply.github.com>
2022-10-20 14:15:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Homework 9
+
+We look at sets in Lean 4. We include our own library with basic definitions,
+results, and notations for sets. 
---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package hw9 {
+  -- add package configuration options here
+}
+
+lean_lib Sets {
+  -- add library configuration options here
+}
+
+lean_lib Hw9 {
+  -- add library configuration options here
+}
+
